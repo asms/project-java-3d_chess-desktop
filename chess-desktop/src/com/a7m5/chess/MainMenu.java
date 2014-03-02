9526d584bf9fa9dfddea1b6919004da3d672ad23
asms <aseventhmindset@gmail.com>
2014-03-02 01:08:35 -0800

    Stuff.

--- chess-desktop/src/com/a7m5/chess/MainMenu.java
+++ chess-desktop/src/com/a7m5/chess/MainMenu.java
@@ -47,16 +47,11 @@ public class MainMenu {
 		});
 	}
 
-	/**
-	 * Create the application.
-	 */
+
 	public MainMenu() {
 		initialize();
 	}
 
-	/**
-	 * Initialize the contents of the frame.
-	 */
 	private void initialize() {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
